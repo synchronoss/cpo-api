8fe747e3d9d4f15b830534d84622b3c7b0109b40
berryware <berryware@26c7a42c-a214-0410-8a99-c1a46ea20dee>
2013-08-02 14:01:09 +0000

    Added a new test to simulate a retrieve with a lot of records so I can check timings

---
+++ cpo-jdbc/src/test/java/org/synchronoss/cpo/jdbc/BigRetrieveTest.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2003-2012 David E. Berry
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * A copy of the GNU Lesser General Public License may also be found at
+ * http://www.gnu.org/licenses/lgpl.txt
+ */
+package org.synchronoss.cpo.jdbc;
+
+import junit.framework.TestCase;
+import org.slf4j.*;
+import org.synchronoss.cpo.*;
+import org.synchronoss.cpo.helper.ExceptionHelper;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
+
+/**
+ * BigBatchTest is a JUnit test class for testing big batches
+ *
+ * @author david berry
+ */
+public class BigRetrieveTest extends TestCase {
+
+  private static final Logger logger = LoggerFactory.getLogger(BigRetrieveTest.class);
+  private ArrayList<ValueObject> al = new ArrayList<ValueObject>();
+  private CpoAdapter cpoAdapter = null;
+
+  public BigRetrieveTest(String name) {
+    super(name);
+  }
+
+  /**
+   * <code>setUp</code> Load the datasource from the properties in the property file jdbc_en_US.properties
+   *
+   * @author david berry
+   * @version '$Id: InsertObjectTest.java,v 1.3 2006/01/30 19:09:23 dberry Exp $'
+   */
+  @Override
+  public void setUp() {
+    String method = "setUp:";
+
+    try {
+      cpoAdapter = CpoAdapterFactory.getCpoAdapter(JdbcStatics.ADAPTER_CONTEXT_JDBC);
+      assertNotNull(method + "IdoAdapter is null", cpoAdapter);
+    } catch (Exception e) {
+      fail(method + e.getMessage());
+    }
+  }
+
+  /**
+   * So oracle seems to fail on a batch size of 100,000 but does not throw an error.
+   *
+   * lets try to break it to fix it to return a good message.
+   *
+   */
+  public void testBigRetrieve() {
+
+    String method = "testBigRetrieve:";
+    int numInserts = 10000;
+
+    for (int i = 0; i < numInserts; i++) {
+      al.add(new ValueObject(i));
+    }
+
+    try {
+      long inserts = cpoAdapter.insertObjects(al);
+      assertEquals("inserts performed do not equal inserts requested: ", inserts, numInserts);
+    } catch (CpoException ce) {
+      logger.debug("Received a CpoException:" + ExceptionHelper.getLocalizedMessage(ce));
+    } catch (Exception e) {
+      logger.error(ExceptionHelper.getLocalizedMessage(e));
+      fail(method + ":Received an Exception instead of a CpoException: " + ExceptionHelper.getLocalizedMessage(e));
+    } catch (Throwable t) {
+      logger.error(ExceptionHelper.getLocalizedMessage(t));
+      fail(method + ":Received a Throwable instead of a CpoException: " + ExceptionHelper.getLocalizedMessage(t));
+    }
+
+    Collection<ValueObject> col;
+    
+    try {
+      ValueObject valObj = new ValueObject();
+      col = cpoAdapter.retrieveBeans(null, valObj);
+      assertTrue("Col size is " + col.size(), col.size() == al.size());
+
+    } catch (Exception e) {
+      fail(method + e.getMessage());
+    }
+  }
+
+  @Override
+  public void tearDown() {
+    String method = "tearDown:";
+    try {
+      cpoAdapter.deleteObjects(al);
+
+    } catch (Exception e) {
+            logger.error(ExceptionHelper.getLocalizedMessage(e));
+      fail(method + e.getMessage());
+    }
+    cpoAdapter = null;
+  }
+}

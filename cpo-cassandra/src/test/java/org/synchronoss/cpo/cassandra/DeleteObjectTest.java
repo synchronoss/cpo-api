0191e433d44fb9b8836685626b5eef39cd180f1b
berryware <berryware@26c7a42c-a214-0410-8a99-c1a46ea20dee>
2013-10-15 21:16:36 +0000

    DeleteObjectTest is working checking it in

---
+++ cpo-cassandra/src/test/java/org/synchronoss/cpo/cassandra/DeleteObjectTest.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2003-2012 David E. Berry
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * A copy of the GNU Lesser General Public License may also be found at
+ * http://www.gnu.org/licenses/lgpl.txt
+ */
+package org.synchronoss.cpo.cassandra;
+
+import junit.framework.TestCase;
+import org.slf4j.*;
+import org.synchronoss.cpo.*;
+import org.synchronoss.cpo.cassandra.meta.CassandraCpoMetaDescriptor;
+import org.synchronoss.cpo.helper.ExceptionHelper;
+
+import java.sql.Timestamp;
+import java.util.*;
+
+/**
+ * DeleteObjectTest is a JUnit test class for testing the JdbcAdapter deleteObject method
+ *
+ * @author david berry
+ */
+public class DeleteObjectTest extends TestCase {
+
+  private static final Logger logger = LoggerFactory.getLogger(DeleteObjectTest.class);
+  private ArrayList<ValueObject> al = new ArrayList<ValueObject>();
+  private CpoAdapter cpoAdapter = null;
+  private CassandraCpoMetaDescriptor metaDescriptor = null;
+
+  public DeleteObjectTest(String name) {
+    super(name);
+  }
+
+  /**
+   * <code>setUp</code> Load the datasource from the properties in the property file jdbc_en_US.properties
+   *
+   * @author david berry
+   * @version '$Id: InsertObjectTest.java,v 1.3 2006/01/30 19:09:23 dberry Exp $'
+   */
+  @Override
+  public void setUp() {
+    String method = "setUp:";
+
+    try {
+      cpoAdapter = CpoAdapterFactory.getCpoAdapter(CassandraStatics.ADAPTER_CONTEXT_DEFAULT);
+      assertNotNull(method + "IdoAdapter is null", cpoAdapter);
+      metaDescriptor = (CassandraCpoMetaDescriptor) cpoAdapter.getCpoMetaDescriptor();
+    } catch (Exception e) {
+      fail(method + e.getMessage());
+    }
+  }
+
+  public void testDeleteObject() {
+    String method = "testDeleteObject:";
+    ValueObject valObj = new ValueObject(5);
+
+    valObj.setAttrVarChar("testDelete");
+    valObj.setAttrInt(3);
+    Date ts = new Timestamp(System.currentTimeMillis());
+
+//    if (!metaDescriptor.isSupportsMillis()) {
+//      ts.setNanos(0);
+//    }
+
+    valObj.setAttrTimestamp(ts);
+
+    valObj.setAttrBool(true);
+
+    al.add(valObj);
+
+    try {
+      cpoAdapter.insertObject(valObj);
+    } catch (Exception e) {
+      logger.error(ExceptionHelper.getLocalizedMessage(e));
+      fail(method + e.getMessage());
+    }
+
+    // try the where on the delete, should delete 0
+    try {
+      long deleted = cpoAdapter.deleteObject(valObj);
+    } catch (Exception e) {
+      logger.error(ExceptionHelper.getLocalizedMessage(e));
+      fail(method + e.getMessage());
+    }
+
+  }
+
+  @Override
+  public void tearDown() {
+    String method = "tearDown:";
+    try {
+      cpoAdapter.deleteObjects(al);
+
+    } catch (Exception e) {
+      logger.error(ExceptionHelper.getLocalizedMessage(e));
+      fail(method + e.getMessage());
+    }
+    cpoAdapter = null;
+  }
+}

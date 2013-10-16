4f1a64bca74221f6551ca8b2f0964ec85717411e
berryware <berryware@26c7a42c-a214-0410-8a99-c1a46ea20dee>
2013-10-16 00:52:04 +0000

    InsertObjectTest is now working

---
+++ cpo-cassandra/src/test/java/org/synchronoss/cpo/cassandra/InsertObjectTest.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2003-2012 David E. Berry
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * A copy of the GNU Lesser General Public License may also be found at
+ * http://www.gnu.org/licenses/lgpl.txt
+ */
+package org.synchronoss.cpo.cassandra;
+
+import junit.framework.TestCase;
+import org.synchronoss.cpo.*;
+import org.synchronoss.cpo.cassandra.meta.CassandraCpoMetaDescriptor;
+
+import java.sql.Timestamp;
+import java.util.*;
+
+/**
+ * InsertObjectTest is a JUnit test class for testing the insert api calls of cpo
+ *
+ * @author david berry
+ */
+public class InsertObjectTest extends TestCase {
+
+  private ArrayList<ValueObject> al = new ArrayList<ValueObject>();
+  private CpoAdapter cpoAdapter = null;
+  private CpoAdapter readAdapter = null;
+  private CassandraCpoMetaDescriptor metaDescriptor = null;
+
+  public InsertObjectTest(String name) {
+    super(name);
+  }
+
+  /**
+   * <code>setUp</code> Load the datasource from the properties in the property file jdbc_en_US.properties
+   *
+   * @author david berry
+   * @version '$Id: InsertObjectTest.java,v 1.3 2006/01/30 19:09:23 dberry Exp $'
+   */
+  @Override
+  public void setUp() {
+    String method = "setUp:";
+
+    try {
+      cpoAdapter = CpoAdapterFactory.getCpoAdapter(CassandraStatics.ADAPTER_CONTEXT_DEFAULT);
+      assertNotNull(method + "IdoAdapter is null", cpoAdapter);
+      metaDescriptor = (CassandraCpoMetaDescriptor) cpoAdapter.getCpoMetaDescriptor();
+    } catch (Exception e) {
+      fail(method + e.getMessage());
+    }
+    try {
+      readAdapter = CpoAdapterFactory.getCpoAdapter(CassandraStatics.ADAPTER_CONTEXT_DEFAULT);
+      assertNotNull(method + "IdoAdapter is null", readAdapter);
+    } catch (Exception e) {
+      fail(method + e.getMessage());
+    }
+  }
+
+  public void testInsertObject() {
+    String method = "testInsertObject:";
+    ValueObject valObj = new ValueObject(5);
+
+    valObj.setAttrVarChar("testInsert");
+    valObj.setAttrInt(3);
+    Date ts = new Timestamp(System.currentTimeMillis());
+
+//    if (!metaDescriptor.isSupportsMillis()) {
+//      ts.setNanos(0);
+//    }
+
+    valObj.setAttrTimestamp(ts);
+
+    valObj.setAttrBool(true);
+
+    al.add(valObj);
+
+    try {
+      cpoAdapter.insertObject(valObj);
+    } catch (Exception e) {
+      fail(method + e.getMessage());
+    }
+
+    try {
+      ValueObject vo = readAdapter.retrieveBean(null, valObj, valObj, null, null);
+      assertTrue("Ids do not match", vo.getId() == valObj.getId());
+      assertTrue("Integers do not match", vo.getAttrInt() == valObj.getAttrInt());
+      assertEquals("Strings do not match", vo.getAttrVarChar(), valObj.getAttrVarChar());
+      assertEquals("Timestamps do not match", vo.getAttrTimestamp(), valObj.getAttrTimestamp());
+      assertTrue("boolean not stored correctly", vo.getAttrBool());
+
+    } catch (Exception e) {
+      fail(method + e.getMessage());
+    }
+
+
+  }
+
+  public void testInsertObjects() {
+
+    String method = "testInsertObjects:";
+    ValueObject vo = new ValueObject(1);
+    vo.setAttrVarChar("Test");
+
+    al.add(vo);
+    al.add(new ValueObject(2));
+    al.add(new ValueObject(3));
+    al.add(new ValueObject(4));
+    try {
+      cpoAdapter.insertObjects(al);
+    } catch (Exception e) {
+      fail(method + e.getMessage());
+    }
+
+    try {
+      Collection<ValueObject> col = readAdapter.retrieveBeans(null, vo);
+
+      assertEquals(method + "Invalid number of objects returned", col.size(), al.size());
+    } catch (Exception e) {
+      fail(method + e.getMessage());
+    }
+
+
+  }
+
+  @Override
+  public void tearDown() {
+    String method = "tearDown:";
+    try {
+      cpoAdapter.deleteObjects(al);
+
+    } catch (Exception e) {
+      fail(method + e.getMessage());
+    }
+    cpoAdapter = null;
+    readAdapter = null;
+  }
+}

4e7a776ed04e23f11baa645ccaad5fd1b6f3a8b6
berryware <david.e.berry@verizon.net>
2024-12-10 20:14:06 -0500

    upgrade the h2 driver. Correct the datatypes in the test table

--- cpo-jdbc/src/test/resources/h2/initDB.sql
+++ cpo-jdbc/src/test/resources/h2/initDB.sql
@@ -19,97 +19,70 @@
 -- http://www.gnu.org/licenses/lgpl.txt
 --
 
-DROP TABLE value_object IF EXISTS ;
-DROP TABLE lob_test IF EXISTS ;
+DROP TABLE IF EXISTS value_object;
+DROP TABLE IF EXISTS lob_test;
 
 CREATE TABLE IF NOT EXISTS value_object (
-id                      int           UNIQUE,
-attr_int                int           NULL,
-attr_int4               int4          NULL,
-attr_signed             signed        NULL,
-attr_integer            integer       NULL,
-attr_mediumint          mediumint     NULL,
-attr_bit                bit           NULL,
-attr_bool               bool          NULL,
-attr_boolean            boolean       NULL,
-attr_tinyint            tinyint       NULL,
-attr_int2               int2          NULL,
-attr_year               year          NULL,
-attr_smallint           smallint      NULL,
-attr_int8               int8          NULL,
-attr_bigint             bigint        NULL,
-attr_identity           identity      PRIMARY KEY,
-attr_dec                dec           NULL,
-attr_decimal            decimal(10,0) NULL,
-attr_number             number        NULL,
-attr_numeric            numeric(10,0) NULL,
-attr_double             double        NULL,
-attr_floatdouble        float(40)     NULL,
-attr_float8             float8        NULL,
-attr_real               real          NULL,
-attr_floatreal          float(20)     NULL,
-attr_float4             float4        NULL,
-attr_time               time          NULL,
-attr_time_without_timezone time without time zone NULL,
-attr_time_with_timezone time with time zone NULL,
-attr_date               date          NULL,
-attr_datetime           datetime      NULL,
-attr_timestamp          timestamp     NULL,
-attr_timestamp_without_timezone    timestamp without time zone NULL,
-attr_smalldatetime      smalldatetime NULL,
-attr_timestamp_with_timezone  timestamp with time zone NULL,
-attr_binary             binary        NULL,
-attr_varbinary          varbinary     NULL,
-attr_binary_varying     binary varying NULL,
-attr_longvarbinary      longvarbinary  NULL,
-attr_raw                raw           NULL,
-attr_bytea              bytea         NULL,
-attr_other              other         NULL,
-attr_varchar            varchar(255)  NULL,
-attr_character_varying  character varying(255)  NULL,
-attr_longvarchar        longvarchar(2048)  NULL,
-attr_varchar2           varchar2(255)  NULL,
-attr_nvarchar           nvarchar(255)  NULL,
-attr_nvarchar2          nvarchar2(255)  NULL,
-attr_varchar_casesensitive varchar_casesensitive(255)  NULL,
-attr_varchar_ignorecase varchar_ignorecase(255)  NULL,
-attr_char		            char(255)     NULL,
-attr_character		      character(255)     NULL,
-attr_nchar		          nchar(255)     NULL,
-attr_blob               blob          NULL,
-attr_binary_large_object  binary large object NULL,
-attr_tinyblob           tinyblob      NULL,
-attr_mediumblob         mediumblob    NULL,
-attr_longblob           longblob      NULL,
-attr_image              image         NULL,
-attr_oid                oid           NULL,
-attr_clob               clob          NULL,
-attr_character_large_object character large object NULL,
-attr_text               text          NULL,
-attr_tinytext           tinytext      NULL,
-attr_mediumtext         mediumtext    NULL,
-attr_longtext           longtext      NULL,
-attr_ntext              ntext         NULL,
-attr_nclob              nclob         NULL,
-attr_uuid               uuid          NULL,
-attr_array              array         NULL,
-attr_enum               enum('club','diamonds','hearts','spades'),
-attr_geometry           geometry      NULL,
-attr_geometry2          geometry      NULL,
-attr_json               json          NULL,
-attr_interval_year      INTERVAL YEAR NULL,
-attr_interval_month     INTERVAL MONTH NULL,
-attr_interval_day       INTERVAL DAY NULL,
-attr_interval_hour      INTERVAL HOUR NULL,
-attr_interval_minute    INTERVAL MINUTE NULL,
-attr_interval_second    INTERVAL SECOND NULL,
-attr_interval_year_to_month INTERVAL YEAR TO MONTH NULL,
-attr_interval_day_to_hour   INTERVAL DAY TO HOUR NULL,
-attr_interval_day_to_minute INTERVAL DAY TO MINUTE NULL,
-attr_interval_day_to_second INTERVAL DAY TO SECOND NULL,
-attr_interval_hour_to_minute INTERVAL HOUR TO MINUTE NULL,
-attr_interval_hour_to_second INTERVAL HOUR TO SECOND NULL,
-attr_interval_minute_to_second INTERVAL MINUTE TO SECOND NULL
+  id                      int           PRIMARY KEY
+  ,attr_int                int           NULL
+  ,attr_integer            integer       NULL
+  ,attr_bit                boolean       NULL
+  ,attr_bool                boolean       NULL
+  ,attr_boolean            boolean       NULL
+  ,attr_tinyint            tinyint       NULL
+  ,attr_smallint           smallint      NULL
+  ,attr_bigint             bigint        NULL
+  ,attr_dec                dec           NULL
+  ,attr_decimal            decimal(10,0) NULL
+  ,attr_numeric            numeric(10,0) NULL
+  ,attr_double             double precision NULL
+  ,attr_floatdouble        float(40)     NULL
+  ,attr_real               real          NULL
+  ,attr_floatreal          float(20)     NULL
+  ,attr_time               time          NULL
+  ,attr_time_without_timezone time without time zone NULL
+  ,attr_time_with_timezone time with time zone NULL
+  ,attr_date               date          NULL
+  ,attr_datetime           timestamp     NULL
+  ,attr_timestamp          timestamp     NULL
+  ,attr_timestamp_without_timezone    timestamp without time zone NULL
+  ,attr_timestamp_with_timezone  timestamp with time zone NULL
+  ,attr_binary             binary        NULL
+  ,attr_varbinary          varbinary     NULL
+  ,attr_binary_varying     binary varying NULL
+  ,attr_other              other         NULL
+  ,attr_object             object         NULL
+  ,attr_java_object        java_object         NULL
+  ,attr_varchar            varchar(255)  NULL
+  ,attr_character_varying  character varying(255)  NULL
+  ,attr_varchar_casesensitive varchar_casesensitive(255)  NULL
+  ,attr_varchar_ignorecase varchar_ignorecase(255)  NULL
+  ,attr_char		            char(255)     NULL
+  ,attr_character		      character(255) NULL
+  ,attr_nchar		          nchar(255)     NULL
+  ,attr_blob               blob          NULL
+  ,attr_binary_large_object  binary large object NULL
+  ,attr_clob               clob          NULL
+  ,attr_character_large_object character large object NULL
+  ,attr_nclob              nclob         NULL
+  ,attr_uuid               uuid          NULL
+  ,attr_array              boolean array         NULL
+  ,attr_enum               enum('club','diamonds','hearts','spades')
+  ,attr_geometry           geometry      NULL
+  ,attr_json               json          NULL
+  ,attr_interval_year      INTERVAL YEAR
+  ,attr_interval_month     INTERVAL MONTH NULL
+  ,attr_interval_day       INTERVAL DAY NULL
+  ,attr_interval_hour      INTERVAL HOUR NULL
+  ,attr_interval_minute    INTERVAL MINUTE NULL
+  ,attr_interval_second    INTERVAL SECOND NULL
+  ,attr_interval_year_to_month INTERVAL YEAR TO MONTH NULL
+  ,attr_interval_day_to_hour   INTERVAL DAY TO HOUR NULL
+  ,attr_interval_day_to_minute INTERVAL DAY TO MINUTE NULL
+  ,attr_interval_day_to_second INTERVAL DAY TO SECOND NULL
+  ,attr_interval_hour_to_minute INTERVAL HOUR TO MINUTE NULL
+  ,attr_interval_hour_to_second INTERVAL HOUR TO SECOND NULL
+  ,attr_interval_minute_to_second INTERVAL MINUTE TO SECOND NULL
 );
 
 CREATE TABLE IF NOT EXISTS lob_test (

d5906e65ee23f0e7e6a4ec202a85b193e08046e3
berryware <david.e.berry@verizon.net>
2025-11-15 07:50:57 -0500

    PMD did not like the obnoxious start end and section markers

--- cpo-core/src/main/java/org/synchronoss/cpo/exporter/CpoLegacyClassSourceGenerator.java
+++ cpo-core/src/main/java/org/synchronoss/cpo/exporter/CpoLegacyClassSourceGenerator.java
@@ -1,11 +1,11 @@
 package org.synchronoss.cpo.exporter;
 
 /*-
- * [-------------------------------------------------------------------------
+ * [[
  * core
- * --------------------------------------------------------------------------
+ * ==
  * Copyright (C) 2003 - 2025 David E. Berry
- * --------------------------------------------------------------------------
+ * ==
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -19,7 +19,7 @@ package org.synchronoss.cpo.exporter;
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- * --------------------------------------------------------------------------]
+ * ]]
  */
 
 import org.synchronoss.cpo.meta.CpoMetaDescriptor;

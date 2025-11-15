7429ecf317dbdd0c076701553760002b8147ca2d
berryware <david.e.berry@verizon.net>
2025-11-15 07:40:48 -0500

    update headers with obnoxious start end and section markers

--- cpo-jdbc/src/main/java/org/synchronoss/cpo/jdbc/JdbcCpoWhere.java
+++ cpo-jdbc/src/main/java/org/synchronoss/cpo/jdbc/JdbcCpoWhere.java
@@ -1,11 +1,11 @@
 package org.synchronoss.cpo.jdbc;
 
 /*-
- * #%L
+ * [-------------------------------------------------------------------------
  * jdbc
- * %%
+ * --------------------------------------------------------------------------
  * Copyright (C) 2003 - 2025 David E. Berry
- * %%
+ * --------------------------------------------------------------------------
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -19,7 +19,7 @@ package org.synchronoss.cpo.jdbc;
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- * #L%
+ * --------------------------------------------------------------------------]
  */
 
 import org.synchronoss.cpo.BindableCpoWhere;
